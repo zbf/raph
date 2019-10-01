efcd4fb92513942b9fcfb59ec730fa0baf993359
Fabio Barcelos <zbarcelos@gmail.com>
2019-10-01 22:36:34 +0200

    first commit

---
+++ src/main/java/com/home/raph/RaphApplication.java
@@ -0,0 +1,13 @@
+package com.home.raph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaphApplication.class, args);
+	}
+
+}
